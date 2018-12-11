56a1f090e9fd6f2636fc9c45b55b15c9c1a0cf98
yangguang <dengyangguang_x@163.com>
2018-12-11 21:01:03 +0800

    branch1 update

--- test/src/main/java/TestGit.java
+++ test/src/main/java/TestGit.java
@@ -2,5 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("2222222");
+        System.out.println("3333333branch");
+        System.out.println("4444444branch");
     }
 }
